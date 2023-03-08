88565961c7ba0c377e3cee695129da0a39ca506c
sheng <1970668895@qq.com>
2023-03-09 00:47:56 +0800

    new_pro

--- case/conftest.py
+++ case/conftest.py
@@ -1,19 +1,48 @@
 import time
 import pytest
+import platform
 from selenium import webdriver
 from pages.login_page import LoginPage
-from common.driver_headless import d_hless
+
+
+def pytest_addoption(parser):
+    ''' pytest钩子函数，自定义命令行输入'''
+    parser.addoption('--headless',
+                     action = 'store',
+                     default = 'no',
+                     help = 'set chrome headless option yes or no')
 
 # 全局fixture
 @pytest.fixture(scope='session')
 def driver(request):
     ''' 定义全局driver '''
-    # _driver = webdriver.Chrome()
-    # _driver.maximize_window()    # 最大化窗口
+    # 判断操作系统
+    # if platform.system() == 'Windows':
+    #     _driver = webdriver.Chrome()
+    # elif platform.system() == 'Darwin':
+    #     _driver = webdriver.Chrome()
+    # elif platform.system() == 'Linux':
+    #     chrome_options = webdriver.ChromeOptions()
+    #     chrome_options.add_argument('--headless')
+    #     chrome_options.add_argument('--no-sandbox')
+    #     chrome_options.add_argument('--disable-gpu')    # 禁用GPU硬件加速
+    #     chrome_options.add_argument('--disable-dev-shm-usage')
+    #     chrome_options.add_argument('--window-size=1920,1080')    # 设置当前窗口宽度和高度
+    #     _driver = webdriver.Chrome('chromedriver', options=chrome_options)
 
-    # 无头模式
-    d_h = d_hless()
-    _driver = d_h.headless_driver()
+    # config.getoption()    读取自定义参数值
+    headless = request.config.getoption('--headless')
+    if headless == 'yes':
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-gpu')    # 禁用GPU硬件加速
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--window-size=1920,1080')    # 设置当前窗口宽度和高度
+        _driver = webdriver.Chrome('chromedriver', options=chrome_options)
+    else:
+        _driver = webdriver.Chrome()
+
 
     def end():
         ''' 测试用例完成后，执行终结函数 '''
